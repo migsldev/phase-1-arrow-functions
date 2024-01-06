5986053beb21d3f36e4d043c351e0eac02993820
Miguel Leonar <migsldev@gmail.com>
2024-01-06 11:10:44 +1100

    Passed all 6 tests

---
+++ index.js
@@ -0,0 +1,14 @@
+//Divide
+const divide = (a, b) => a / b;
+
+console.log(divide(2000,100));
+
+//Squared
+const square = (x) => x ** x;
+
+console.log(square(2));
+
+//Add
+const add = (a, b) => a + b;
+
+console.log(add(3, 4));
